f46b88a6f3c1bbc74c683267360768021d28eda3
Joshua <joshkap2015@gmail.com>
2024-02-17 18:54:36 +0200

    Holy shit

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
-import { defineConfig, type UserConfig } from "vite";
+import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
+import { builderDevTools } from "@builder.io/dev-tools/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig((): UserConfig => {
+export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
-    server: {
-      headers: {
-        "Cache-Control": "public, max-age=0",
-      },
-    },
-    preview: {
-      headers: {
-        "Cache-Control": "public, max-age=600",
-      },
-    },
+    plugins: [builderDevTools(), qwikCity(), qwikVite(), tsconfigPaths()],
   };
 });
