a52902ebc2367158505c4da542bfed9139557863
Joshua <joshkap2015@gmail.com>
2024-02-17 18:31:33 +0200

    Fixed static shit

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -1,4 +1,4 @@
-import { cloudflarePagesAdapter } from "@builder.io/qwik-city/adapters/cloudflare-pages/vite";
+import { staticAdapter } from "@builder.io/qwik-city/adapters/static/vite";
 import { extendConfig } from "@builder.io/qwik-city/vite";
 import baseConfig from "../../vite.config";
 
@@ -7,9 +7,13 @@ export default extendConfig(baseConfig, () => {
     build: {
       ssr: true,
       rollupOptions: {
-        input: ["src/entry.cloudflare-pages.tsx", "@qwik-city-plan"],
+        input: ["@qwik-city-plan"],
       },
     },
-    plugins: [cloudflarePagesAdapter()],
+    plugins: [
+      staticAdapter({
+        origin: "https://yoursite.qwik.dev",
+      }),
+    ],
   };
 });
